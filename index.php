001e61307d1dbc77c7bf1b71ba83ae99c5e807b1
Leoncides Ferreira da silva Neto <125755674+Leoncides@users.noreply.github.com>
2023-03-24 14:11:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require_once 'cabecalho.php'?>
+<div id="topo">
+	<div id="logo">		
+	
+	<a href="index.php"><img src="img/logo.png"></a></div>
+<div id="menu">
+	<ul class="nav">
+		<li><a href="curriculum.php" target="quadro"><button class="top">Curriculum</button> </a></li>
+		<li><a href="skills.php" target="quadro"><button class="top">Skills</button></a></li>
+		<li><a href="linkes.php" target="quadro"><button class="top">Links</button></a></li>
+	</ul>
+</div>
+</div>
+    <div id="principal">
+	<iframe src="home.php" name="quadro"></iframe>
+</div>
+<div id="rodape">
+	<div id="contatos">
+		<h2><button class="top">Phone:+55(42)988881312</button></h2>
+		<h2><a href="mailto:leoncidess@gmail.com"><button class="top">E-mail:leoncidess@gmail.com</button></a><h2>
+	</div>
+	<div id="lin">
+		<h2><a href="https://www.linkedin.com/in/leoncides-ferreira-da-silva-neto-634394109/"><button class="top">Linkedin</button></a></h2>
+		<h2><a href="https://github.com/Leoncides"><button class="top">Github</button></a></h2>
+	</div>
+</div>
+</body>
+</html>
